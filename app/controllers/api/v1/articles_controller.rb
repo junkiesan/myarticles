cbcd1768ee1fea89f65b77345f92aaccbc6f4894
Julien Bouland <bouland.julien@gmail.com>
2020-12-08 18:57:19 +0100

    bug route fixed

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,9 +1,9 @@
 module Api
-  module v1
+  module V1
     class ArticlesController < ApplicationController
       def index
         articles = Article.order('created_at DESC');
-        render json: {status: 'SUCCESS', message: 'Loaded articles', data:articles}, status:ok
+        render json: {status: 'SUCCESS', message: 'Loaded articles', data:articles}, status: :ok
       end
       
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace 'v1' do
-      ressources :articles
+      resources :articles
     end
   end
 end
